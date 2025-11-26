a02300b2f802387476b53302e822f232c49b6f31
Gulnihalv <gulnihal.vurat@st.uskudar.edu.tr>
2025-11-26 23:28:52 +0300

    Dosyalar oluşturuldu

---
+++ config/paths.py
@@ -0,0 +1,15 @@
+import os
+from pathlib import Path
+
+IS_COLAB = os.path.exists('/content/drive')
+
+if IS_COLAB:
+    ROOT_DIR = Path('/content/drive/MyDrive/Crypto-Project')
+else:
+    # Yerel bilgisayar (Repo'nun kök dizini)
+    ROOT_DIR = Path(__file__).parent.parent 
+
+DATA_DIR = ROOT_DIR / "data"
+RAW_DATA_DIR = DATA_DIR / "raw"
+PROCESSED_DATA_DIR = DATA_DIR / "data/processed"
+# MODEL_DIR = ROOT_DIR / "models_saved"
